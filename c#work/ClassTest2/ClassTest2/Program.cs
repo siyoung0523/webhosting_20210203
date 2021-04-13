1648917f8e81b2609eccd2ba98233798a9d366e2
siyoung0523 <cookee23@naver.com>
2021-04-13 09:24:28 +0900

    0413

---
+++ c#work/ClassTest2/ClassTest2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ jsp/20210412jsp/src/com/mh/Ex01.java
@@ -0,0 +1,12 @@
+package com.mh;
+
+public class Ex01 {
+
+	public void doA() {
+		System.out.println("doA함수 입니다.");
+	}
+	
+	public String reString() {
+		return "reString 반환";
+	}
+}
